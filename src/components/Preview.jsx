6a24fb83edc4fd0c8ee192de912ccdedbb211c6b
davidpounds <david@davidpounds.com>
2021-10-01 23:40:14 +0100

    Adds a sticky preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,5 +1,4 @@
 import './Preview.css';
-import EditWrapper, { DISPLAY_MODE } from './EditWrapper';
 
 const Preview = props => {
     const { theme } = props;
@@ -8,146 +7,26 @@ const Preview = props => {
     return (
         <div className={`preview ${theme}`}>
             <div className="preview-heading">Preview - {theme} theme</div>
-            <EditWrapper 
-                properties={[
-                    "--site-background-colour", 
-                    "--primary-brand-colour", 
-                    "--secondary-brand-colour"
-                ]}
-            >
-                <EditWrapper 
-                    properties={[
-                        "--page-header-colour"
-                    ]}
-                >
-                    <header>
-                        <EditWrapper 
-                            theme={theme} 
-                            properties={[
-                                "--page-header-logo", 
-                                "--page-header-logo-width", 
-                                "--page-header-logo-height"
-                            ]
-                        }>
-                            <div className="preview-header-logo" title="Logo preview" />
-                        </EditWrapper>
-                        <EditWrapper 
-                            properties={["--page-header-text-colour"]}
-                        >
-                            <p>
-                                Header text with a<> </> 
-                                <EditWrapper
-                                    display={DISPLAY_MODE.INLINE} 
-                                    properties={["--page-header-link-colour"]}
-                                >
-                                    <a href="#top" onClick={doNothing}>link</a>
-                                </EditWrapper>
-                                .
-                            </p>
-                        </EditWrapper>
-                    </header>
-                </EditWrapper>
-                <EditWrapper
-                    properties={[
-                        "--heading1-size",
-                        "--heading1-colour"
-                    ]}
-                >
-                    <h1>Heading 1</h1>
-                </EditWrapper>
-                <EditWrapper
-                    properties={[
-                        "--heading2-size",
-                        "--heading2-colour"
-                    ]}
-                >
-                    <h2>Heading 2</h2>
-                </EditWrapper>
-                <EditWrapper
-                    properties={[
-                        "--heading3-size",
-                        "--heading3-colour"
-                    ]}
-                >
-                    <h3>Heading 3</h3>
-                </EditWrapper>
-                <EditWrapper
-                    properties={[
-                        "--heading4-size",
-                        "--heading4-colour"
-                    ]}
-                >
-                    <h4>Heading 4</h4>
-                </EditWrapper>
-                <EditWrapper
-                    properties={[
-                        "--body-text-size",
-                        "--body-text-colour"                    
-                    ]}
-                >
-                    <p>
-                        Body text with a<> </>
-                        <EditWrapper
-                            display={DISPLAY_MODE.INLINE} 
-                            properties={["--body-link-colour"]}
-                        >
-                            <a href="#top" onClick={doNothing}>link</a>
-                        </EditWrapper>
-                        .
-                    </p>
-                </EditWrapper>
-                <p>
-                    <EditWrapper 
-                        display={DISPLAY_MODE.INLINE}
-                        properties={[
-                            "--btn-primary-background-colour",
-                            "--btn-primary-text-colour"
-                        ]}
-                    >
-                        <button className="button button-primary">Primary button</button>
-                    </EditWrapper>
-                </p>
-                <p>
-                    <EditWrapper 
-                        display={DISPLAY_MODE.INLINE}
-                        properties={[
-                            "--btn-secondary-background-colour",
-                            "--btn-secondary-text-colour"
-                        ]}
-                    >
-                        <button className="button button-secondary">Secondary button</button>
-                    </EditWrapper>
-                </p>
-                <p>
-                    <EditWrapper 
-                        display={DISPLAY_MODE.INLINE}
-                        properties={[
-                            "--btn-option-active-background-colour",
-                            "--btn-option-active-text-colour"
-                        ]}
-                    >
-                        <button className="button button-option active">Option (active)</button>
-                    </EditWrapper>
-                    <EditWrapper 
-                        display={DISPLAY_MODE.INLINE}
-                        properties={[
-                            "--btn-option-inactive-background-colour",
-                            "--btn-option-inactive-text-colour"                            
-                        ]}
-                    >
-                        <button className="button button-option">Option (inactive)</button>
-                    </EditWrapper>
-                    <EditWrapper 
-                        display={DISPLAY_MODE.INLINE}
-                        properties={[
-                            "--btn-option-inactive-background-colour",
-                            "--btn-option-inactive-text-colour"                            
-                        ]}
-                    >
-                        <button className="button button-option">Option (inactive)</button>
-                    </EditWrapper>
-                </p>
-            </EditWrapper>
+            <header className="page-heading" data-section="Page header">
+                <div className="preview-header-logo" title="Logo preview" data-section="Logo" />
+                <p>Header text with a <a href="#top" onClick={doNothing}>link</a>.</p>
+            </header>
+            <div className="typography">
+                <h1>Heading 1</h1>
+                <h2>Heading 2</h2>
+                <h3>Heading 3</h3>
+                <h4>Heading 4</h4>
+                <p>Body text with a <a href="#top" onClick={doNothing}>link</a>.</p>
+            </div>
+            <p>
+                <button className="button button-primary">Primary button</button>
+                <button className="button button-secondary">Secondary button</button>
+            </p>
+            <p>
+                <button className="button button-option active">Option (active)</button>
+                <button className="button button-option">Option (inactive)</button>
+                <button className="button button-option">Option (inactive)</button>
+            </p>
         </div>
     );
 };
--- src/components/WhiteLabelAdmin.jsx
+++ src/components/WhiteLabelAdmin.jsx
@@ -1,20 +1,21 @@
 import './WhiteLabelAdmin.css';
 import { useSelector } from 'react-redux';
-import { getWhiteLabelName } from '../selectors';
-import Editor from  './Editor';
+import { getWhiteLabelName, getWhiteLabelSections } from '../selectors';
 import Preview from './Preview';
+import Section from './Section';
 
 const WhiteLabelAdmin = props => {
     const name = useSelector(getWhiteLabelName);
+    const sections = useSelector(getWhiteLabelSections);
 
     return (
         <div className="whitelabel-admin">
             <h1 className="whitelabel-admin-heading">{name}</h1>
-            <Editor />
             <div className="preview-wrapper">
                 <Preview theme="light" />
                 <Preview theme="dark" />
             </div>
+            {sections.map(section => <Section key={section.sectionId} section={section} />)}
         </div>
     );
 };
