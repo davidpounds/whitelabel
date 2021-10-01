b5728d9f5358537c9286eff6cff09a4f524a4fca
davidpounds <david@davidpounds.com>
2021-10-01 12:26:54 +0100

    Removes value sort and uses CSS grid to keep order

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,6 +16,4 @@ export const makeUpperCaseFirstLetter = text => String(text).substr(0, 1).toUppe
 
 export const isThemed = cssValue => cssValue?.constructor !== String && Object.keys(cssValue).every(k => [THEME.LIGHT, THEME.DARK].includes(k));
 
-export const getThemedValues = cssValue => isThemed(cssValue) ? 
-    Object.entries(cssValue).sort((a, b) => a.cssValue > b.cssValue ? 1 : -1) : 
-    [[THEME.GLOBAL, cssValue]];
+export const getThemedValues = cssValue => isThemed(cssValue) ? Object.entries(cssValue) : [[THEME.GLOBAL, cssValue]];
